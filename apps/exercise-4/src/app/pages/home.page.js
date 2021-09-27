e21086382eb52bbe3a58131e4d6559a21d54cee2
hpierre74 <pierrehuyghe.pro@gmail.com>
2021-09-27 19:39:44 +0200

    refactor(exercise-4): migrate ex 4

--- apps/exercise-4/src/app/pages/home.page.js
+++ apps/exercise-4/src/app/pages/home.page.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+
+import { Layout } from '../components/layout.component';
 
 import { getArticles } from '@react-course-v2/api';
 
@@ -14,16 +15,10 @@ export const HomePage = () => {
   }, [articles]);
 
   return (
-    <div>
+    <Layout data-testid="app">
       <h2 data-testid="app-title">Home Page</h2>
-      <Link data-testid="link-to-about" to="/about">
-        About Page
-      </Link>
-      <Link data-testid="link-to-contact" to="/contact">
-        Contact Page
-      </Link>
-      <div>
-        <h4>Articles</h4>
+      <div data-testid="articles-container">
+        <h4 data-testid="articles-title">Articles</h4>
         <ul data-testid="articles-list">
           {articles.length > 0 &&
             articles.map(({ id, name }) => (
@@ -33,6 +28,6 @@ export const HomePage = () => {
             ))}
         </ul>
       </div>
-    </div>
+    </Layout>
   );
 };
