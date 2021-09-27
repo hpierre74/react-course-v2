b47e1ffd9a7a3fdffdbb99bc46778f6940ab4f0e
hpierre74 <pierrehuyghe.pro@gmail.com>
2021-09-27 20:09:10 +0200

    refactor(ex-6): migrate exercise 6

--- README.md
+++ README.md
@@ -233,3 +233,7 @@ If you want to create new exercises, be my guest. It could also be something tot
 
 This course contains some README files for exercises instructions and for theory parts in english.
 I hope a documented system will be in place when you'll read these lines to add translations seamlessly.
+
+TODO:
+
+- Test React Contexts with testing library
--- apps/exercise-6/src/app/pages/home.page.js
+++ apps/exercise-6/src/app/pages/home.page.js
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-import { getArticles } from '@react-course-v2/api';
+import { Layout } from '../components/layout.component';
+import { ArticlesList } from '../modules/articles/components/articlesList.component';
 
 export const HomePage = () => {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    if (articles.length !== 0) {
-      return;
-    }
-    getArticles().then(setArticles).catch(console.error);
-  }, [articles]);
-
   return (
-    <div>
+    <Layout data-testid="app">
       <h2 data-testid="app-title">Home Page</h2>
-      <Link data-testid="link-to-about" to="/about">
-        About Page
-      </Link>
-      <Link data-testid="link-to-contact" to="/contact">
-        Contact Page
-      </Link>
-      <div>
-        <h4>Articles</h4>
-        <ul data-testid="articles-list">
-          {articles.length > 0 &&
-            articles.map(({ id, name }) => (
-              <li data-testid={`article-${id}`} key={id}>
-                {name}
-              </li>
-            ))}
-        </ul>
-      </div>
-    </div>
+      <ArticlesList />
+    </Layout>
   );
 };
